260ee5d6fef3b50f69c67706f84a3e4156745d07
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-26 22:22:49 +0500

    app users update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 // Load config
@@ -8,6 +10,15 @@ dotenv.config({  path: './config/config.env' })
 connectDB()
 
 const app = express()
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 3000
 
--- config/db.js
+++ config/db.js
@@ -5,7 +5,6 @@ const connectDB = async() => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            
         })
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
