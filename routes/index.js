f30238a47bf3ab884e413ad28556f3cf88720f43
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-26 23:34:26 +0500

    css started

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('login')
+    res.render('login', { 
+        layout: 'login',
+    })
 })
 
 
