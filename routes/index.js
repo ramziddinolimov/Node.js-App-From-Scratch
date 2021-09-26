64fffbe6ee334e90928a8a9d8c7797776a526690
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-26 22:38:18 +0500

    index.js started

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,14 @@
-    
+const express = require('express');
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('Login')
+})
+
+
+router.get('/dashboard', (req, res) => {
+    res.send('Dashboard')
+})
+
+
+module.exports = router
