818b9a6d7ab023df4ced8b4fa0446c73096aec2d
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-28 00:55:03 +0500

    passport user.js started and continued

--- config/passport.js
+++ config/passport.js
@@ -2,4 +2,21 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
 
-module.exports = function (passport) {}
+module.exports = function (passport) {
+    passport.use(new GoogleStrategy ({
+        clienID: process.env.GOOGLE_CLIEN_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+    }))
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+      })
+      
+      passport.deserializeUser((id, done) => {
+        User.findById(id,(err, user) => done(err, user))
+      })
+}
