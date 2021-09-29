25d31ff79481413019ff755a382a7a6e439fd26f
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-29 22:36:36 +0500

    auth.js started res render logout

--- routes/auth.js
+++ routes/auth.js
@@ -9,5 +9,10 @@ router.get('/google/callback', passport.authenticate('google', {failureRedirect:
     res.redirect('/dashboard')
 })
 
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
 
 module.exports = router
