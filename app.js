f27dd579c39e1c2e5df030bb1ec33f62eb01502e
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-10-01 01:09:39 +0500

    update session

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 // Load config
