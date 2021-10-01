52373b1e2dd916faf1c2013643bc7f408c37c31c
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-10-01 23:42:24 +0500

    error file upload

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,16 @@ router.get('/', ensureGuest, (req, res) => {
 
 router.get('/dashboard', ensureAuth, async (req, res) => {
     try {
-        const stories = await Story.find({ user: req.user.id })
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
     } catch (err) {
+        console.error(err)
         
     }
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+    
 })
 
 
