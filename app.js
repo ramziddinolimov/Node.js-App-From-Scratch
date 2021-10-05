9397567a7a5eb98a14c0048afbf1823c7109c8b1
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-10-05 21:53:36 +0500

    stories vs story upload

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ connectDB()
 
 const app = express()
 
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
--- routes/stories.js
+++ routes/stories.js
@@ -9,4 +9,17 @@ router.get('/add', ensureAuth, (req, res) => {
 })
 
 
+router.post('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+        
+    }
+})
+
+
 module.exports = router
