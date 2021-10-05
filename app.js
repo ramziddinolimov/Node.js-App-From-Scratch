f56c872c5f92b7355682700de68d851a0c0d0514
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-10-05 22:51:02 +0500

    format date app update

--- app.js
+++ app.js
@@ -28,8 +28,11 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// Handlebars Helpers
+const { formatDate } = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.engine('.hbs', exphbs({helpers: {formatDate,}, defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
 // Sessions
